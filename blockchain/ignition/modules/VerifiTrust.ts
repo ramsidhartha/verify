2bb3fb6c0604b6829ee845112303c329fbd1cd1a
ramsidhartha <ramsidhartha.ch@gmail.com>
2026-02-08 09:28:29 +0530

    Add Polygon Amoy network config for contract deployment

--- blockchain/ignition/modules/VerifiTrust.ts
+++ blockchain/ignition/modules/VerifiTrust.ts
@@ -1,10 +1,8 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
 const VerifiTrustModule = buildModule("VerifiTrustModule", (m) => {
-  // Deploy the contract
   const verifiTrust = m.contract("VerifiTrust");
 
-  // Return the deployed contract instance
   return { verifiTrust };
 });
 
